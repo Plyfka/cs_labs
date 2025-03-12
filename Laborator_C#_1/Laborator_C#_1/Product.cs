f88d79118d66de3335ebcd1193aed01982396876
HG-OlegFedotov <oleg.fedotov@hitek-games.com>
2025-03-12 15:57:28 +0200

    lab_1

---
+++ Laborator_C#_1/Laborator_C#_1/Product.cs
@@ -0,0 +1,66 @@
+﻿using System;
+class Product
+{
+    private string name;
+    private decimal price;
+    private int quantityOfProduct;
+
+    public Product(string name, decimal price, int quantityOfProduct) //конструктор
+    {
+        Name = name;
+        Price = price;
+        this.quantityOfProduct = quantityOfProduct;
+    }
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Назва товару не може бути порожньою!");
+            name = value;
+        }
+    }
+
+    public decimal Price
+    {
+        get => price;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Ціна не може бути від'ємною!");
+            price = value;
+        }
+    }
+
+    public int QuantityOfProduct => quantityOfProduct;
+
+    public decimal TotalValue => price * quantityOfProduct;
+
+    public void AddQuantity(int amount) //метод
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Кількість для поповнення має бути більшою за 0!");
+        quantityOfProduct += amount;
+    }
+
+    public void Sells(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Кількість для продажу має бути більшою за 0!");
+        if (amount > quantityOfProduct)
+        {
+            Console.WriteLine("Недостатньо товару на складі!");
+            return;
+        }
+        quantityOfProduct -= amount;
+    }
+
+    public string GetInfo()
+    {
+        return $"Товар: {Name}, Ціна: {Price} грн, Кількість: {quantityOfProduct}, Загальна вартість: {TotalValue} грн";
+    }
+}
+
+
---
+++ Laborator_C#_1/Laborator_C#_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+
+        Product apple = new Product("Яблуко", 5, 100);
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Sells(20);
+        Console.WriteLine(apple.GetInfo());
+
+        apple.AddQuantity(50);
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Price = 7;
+        Console.WriteLine(apple.GetInfo());
+
+        apple.Name = "Зелене яблуко";
+        Console.WriteLine(apple.GetInfo());
+
+        try
+        {
+            apple.Price = -10;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            apple.Name = "";
+        }
+        catch (Exception er)
+        {
+            Console.WriteLine(er.Message);
+        }
+
+        apple.Sells(200);
+    }
+}
